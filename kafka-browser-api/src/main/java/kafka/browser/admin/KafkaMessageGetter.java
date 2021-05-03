40196fd2fd2843fd19dc57ad8eaa4162a4842a67
Marcin <dziarmagam@gmail.com>
2021-05-03 17:51:47 +0200

    message finder

--- kafka-browser-api/src/main/java/kafka/browser/admin/KafkaMessageGetter.java
+++ kafka-browser-api/src/main/java/kafka/browser/admin/KafkaMessageGetter.java
@@ -5,9 +5,11 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.common.TopicPartition;
 
 import java.time.Duration;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
+import java.time.Instant;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import kafka.browser.connection.KafkaConsumerPool;
 
@@ -49,6 +51,29 @@ public class KafkaMessageGetter {
                     .filter(record -> record.offset() == offset)
                     .findFirst()
                     .map(record -> new String(record.value()));
+        }
+    }
+
+    public List<byte[]> getMessagesWithKey(TopicPartition topicPartition, String key, Instant time, Long endOffset) {
+        try (var kafkaConsumer = kafkaConsumerPool.getConsumerConnection()) {
+            var offsetAndTime = kafkaConsumer.offsetsForTimes(Map.of(topicPartition, time.getEpochSecond()));
+            var offset = offsetAndTime.get(topicPartition).offset();
+            kafkaConsumer.seek(topicPartition, offset);
+            var lastRecord = 0L;
+            var messages = new ArrayList<byte[]>();
+            do {
+                ConsumerRecords<byte[], byte[]> poll = kafkaConsumer.poll(Duration.ofSeconds(1));
+                var recordIterator = poll.iterator();
+                ConsumerRecord<byte[], byte[]> record = null;
+                while (recordIterator.hasNext()) {
+                    record = recordIterator.next();
+                    if (new String(record.key()).equals(key)) {
+                        messages.add(record.value());
+                    }
+                }
+                if (record != null) lastRecord = record.offset();
+            } while (lastRecord < endOffset || lastRecord != 0);
+            return messages;
         }
     }
 }
